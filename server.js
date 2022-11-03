a7772effdcf06fa850ec1ead9cb327d08f1ab17e
Gabriel H. Dalmoro <ghdalmoro@gmail.com>
2022-11-03 13:30:41 -0600

    feat: add weather co nditions API call using browser fetch

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
-import express from 'express';
-import fetch from 'node-fetch';
+// import express from 'express';
 
-const app = express();
-const PORT = 4001;
+// const app = express();
+// const PORT = 4001;
 
-app.listen(PORT, () => {
-  console.log(`The server is running on port ${PORT}. `);
-});
-
-const url =
-  'https://ski-resort-forecast.p.rapidapi.com/Lake%20Louise/forecast?units=m&el=top';
-
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '0a85a08416msh6a3d114f3ace883p1bf07djsnd8c925397308',
-    'X-RapidAPI-Host': 'ski-resort-forecast.p.rapidapi.com',
-  },
-};
-
-fetch(url, options)
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error('error:' + err));
+// app.listen(PORT, () => {
+//   console.log(`The server is running on port ${PORT}. `);
+// });
--- src/index.js
+++ src/index.js
@@ -1,35 +1,28 @@
-import fetch from 'node-fetch';
+// import dotenv from '../node_modules/dotenv';
+// dotenv.config();
 
-//ONLY 10 calls PER DAY!
+let resortMenu = document.querySelector('#resort');
+resortMenu.addEventListener('change', (event) => {
+  getWeather(event.target.value);
+});
 
-// const url =
-//   'https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort/whistler-blackcomb';
+async function getWeather(resortName) {
+  console.log(`fetching ${resortName}`);
+  const url = `https://ski-resort-forecast.p.rapidapi.com/${resortName}/forecast?units=m&el=top`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '0a85a08416msh6a3d114f3ace883p1bf07djsnd8c925397308',
+      //   'X-RapidAPI-Key': process.env.API_KEY,
+      'X-RapidAPI-Host': 'ski-resort-forecast.p.rapidapi.com',
+    },
+  };
+  let outputData = await fetch(url, options)
+    .then((res) => res.json())
+    .then((forecastData) => {
+      return forecastData;
+    })
+    .catch((err) => console.error('error:' + err));
 
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': '0a85a08416msh6a3d114f3ace883p1bf07djsnd8c925397308',
-//     'X-RapidAPI-Host': 'ski-resorts-and-conditions.p.rapidapi.com',
-//   },
-// };
-
-// fetch(url, options)
-//   .then((res) => res.json())
-//   .then((json) => console.log(json))
-//   .catch((err) => console.error('error:' + err));
-
-const url =
-  'https://ski-resort-forecast.p.rapidapi.com/Lake%20Louise/hourly?units=i&el=top&c=false';
-
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '0a85a08416msh6a3d114f3ace883p1bf07djsnd8c925397308',
-    'X-RapidAPI-Host': 'ski-resort-forecast.p.rapidapi.com',
-  },
-};
-
-fetch(url, options)
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error('error:' + err));
+  document.querySelector('#summary').innerHTML = outputData.summary3Day;
+}
